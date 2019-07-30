9e5e751e0ca1fc10fc6d6b28472bca1053b9d9b7
Krzysztof Płaczek <krzysztof.placzek@yahoo.com>
2019-07-30 20:42:07 +0200

    Initial commit

---
+++ src/main/java/ixrk/planetside/kdratio/KdratioApplication.java
@@ -0,0 +1,13 @@
+package ixrk.planetside.kdratio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdratioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KdratioApplication.class, args);
+    }
+
+}
