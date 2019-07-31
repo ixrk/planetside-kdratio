aab87200fd0504edbd9f2031c1b24c6f3f37dbe6
Krzysztof Płaczek <krzysztof.placzek@yahoo.com>
2019-07-31 19:28:08 +0200

    Basic front-end and controller

--- src/main/java/ixrk/planetside2/kdratio/OnlyOneController.java
+++ src/main/java/ixrk/planetside2/kdratio/OnlyOneController.java
@@ -1,16 +1,32 @@
 package ixrk.planetside2.kdratio;
 
+import ixrk.planetside2.kdratio.character.CharactersStatDTO;
+import ixrk.planetside2.kdratio.character.CharactersStatService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
 @Controller
 public class OnlyOneController {
+    private CharactersStatService charactersStatService;
+
+    @Autowired
+    public OnlyOneController(CharactersStatService charactersStatService) {
+        this.charactersStatService = charactersStatService;
+    }
+
     @RequestMapping("/")
     @GetMapping
     public String controller(@RequestParam(required = false) String characterName, Model model) {
+        if (characterName != null) {
+            CharactersStatDTO stats = charactersStatService.statsByCharacterName(characterName);
+            model.addAttribute("stats", stats);
+        }
 
         return "index";
     }
--- src/main/java/ixrk/planetside2/kdratio/character/CharactersStatDTO.java
+++ src/main/java/ixrk/planetside2/kdratio/character/CharactersStatDTO.java
@@ -1,18 +1,24 @@
 package ixrk.planetside2.kdratio.character;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @Getter @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class CharactersStatDTO {
-    private String id;
-    private String characterName;
-    private int kills;
-    private int deaths;
-    private int killDeathRatio;
-    private int killsInMonth;
-    private int deathsInMonth;
-    private int killDeathRatioInMonth;
+    Map<String, String> map = new LinkedHashMap<>();
+//    private String id;
+//    private String characterName;
+//    private int kills;
+//    private int deaths;
+//    private int killDeathRatio;
+//    private int killsInMonth;
+//    private int deathsInMonth;
+//    private int killDeathRatioInMonth;
 }
